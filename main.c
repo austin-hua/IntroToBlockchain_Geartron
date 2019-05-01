fa98a3551ed91771fbbc32a7d9a59ef2124ba824
Seth Austin Harding <gearladeldsvon@gmail.com>
2019-05-01 13:58:09 +0800

    Update main.c

--- main.c
+++ main.c
@@ -1,7 +1,20 @@
 #include <stdio.h>
 
+#include "geartron.h"
+
 const int VERSION = 0x1;
 
+typedef struct BlockHeader {
+    unsigned version;
+    char prevBlock[32]; // hash of previous block
+    char target[32]; // target difficulty
+    unsigned nonce[32];
+} BlockHeader;
+
+
 int main(void) {
-  return 0;
+    
+    
+    
+    return 0;
 }
